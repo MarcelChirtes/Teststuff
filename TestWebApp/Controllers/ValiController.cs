ab1f80cd07259850cfd6d0ec0efb01054e7c90e0
Marcel Chirtes <silenttremor@gmail.com>
2017-01-16 16:37:25 +0200

    Added solution files

---
+++ TestWebApp/Controllers/ValiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using TestWebApp.Models;
+
+namespace TestWebApp.Controllers
+{
+    public class ValiController : ApiController
+    {
+        // POST api/values
+        public IHttpActionResult Post([FromBody]MyModel value)
+        {
+            var serialized = JsonConvert.SerializeObject(value);
+            return Ok(new {test = serialized});
+        }
+    }
+}
---
+++ TestWebApp/Models/MyModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWebApp.Models
+{
+    public class MyModel
+    {
+        public MyPayload payload { get; set; }
+    }
+}
